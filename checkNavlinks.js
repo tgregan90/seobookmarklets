abaf017edd517adaf34ca26f5b74edda132b0fe0
Tom Gregan <tgregan90@hotmail.co.uk>
2021-02-24 11:39:56 +0000

    Added customer user agent and redirect test

--- checkNavlinks.js
+++ checkNavlinks.js
@@ -7,11 +7,15 @@ async function processLinks(linksArray){
 
 async function testLink(link){
     let request = new Request(link);
+    let headers = new Headers({"User-Agent":"MY-UA-STRING"});
 try{
-  await fetch(request).then(response=>{
+  await fetch(request,{
+    method  : 'GET', 
+    headers : headers})
+      .then(response=>{
     console.log(response);
     console.log(response.ok);
-     if(response.ok){
+     if(response.ok && response.redirected == false ){
         link.style.color = "green"
     } else {
         link.style.color = "red"
