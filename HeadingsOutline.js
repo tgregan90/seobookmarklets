b224f91dc7a885132448fc31bd2547d9a6288fe8
Tom Gregan <tgregan90@hotmail.co.uk>
2021-03-09 15:32:03 +0000

    Refactored to make it togglable

--- HeadingsOutline.js
+++ HeadingsOutline.js
@@ -1,37 +1,47 @@
-let headings = document.querySelectorAll("h1,h2,h3,h4,h5,h6");
-for(heading of headings){
-    switch (heading.nodeName) {
-        case "H1":
-            heading.style.backgroundColor = "green";
-            heading.style.color = "white";
-            heading.append(" - (H1)");
-            break;
-        case "H2":
-            heading.style.backgroundColor = "blue";
-            heading.style.color = "white";
-            heading.append(" - (H2)");
-            break;
-        case "H3":
-            heading.style.backgroundColor = "red";  
-            heading.style.color = "white";
-            heading.append(" - (H3)");
-            break;
-        case "H4":
-            heading.style.backgroundColor = "purple"; 
-            heading.style.color = "white"; 
-            heading.append(" - (H4)");
-            break;
-        case "H5":
-            heading.style.backgroundColor = "orange"; 
-            heading.style.color = "white";
-            heading.append(" - (H5)");
-            break;  
-        case "H6":
-            heading.style.backgroundColor = "yellow";  
-            heading.append(" - (H6)");
-            break;            
-            
-        default:
-            break;
+
+document.body.classList.toggle("headingsHighlight");
+
+if(document.querySelector(".headingsHighlight")){
+    if(!window.returnText){window.returnText = tagName => `<span class="headingHighlighter"> - ${tagName.toString()}</span>` }
+    if(!document.querySelector("#headingsHighlightCSS")){
+        const stylesNode = `<style id="headingsHighlightCSS">
+        body.headingsHighlight h1, 
+        body.headingsHighlight h2, 
+        body.headingsHighlight h3, 
+        body.headingsHighlight h4,
+        body.headingsHighlight h5 {
+            color: white;
+        }
+        body.headingsHighlight h1 {
+            background-color: green !important;
+        }
+        body.headingsHighlight h2{
+            background-color : blue !important;
+        }
+        body.headingsHighlight h3{
+            background-color : red !important; 
+        }
+        body.headingsHighlight h4{
+            background-color : purple !important;
+        }
+        body.headingsHighlight h5{
+            background-color : orange !important;
+        }
+        body.headingsHighlight h6{
+            background-color : yellow !important;
+        }
+        </style>`;
+        document.body.insertAdjacentHTML("beforebegin",stylesNode);
+    }
+    
+    let headings = document.querySelectorAll("h1,h2,h3,h4,h5,h6");
+    for(heading of headings){
+        let headingTag = returnText(heading.nodeName);
+        heading.insertAdjacentHTML("beforeend",headingTag);
+    }
+} else {
+    let headingsSpans = document.querySelectorAll(".headingHighlighter");
+    for(headingSpan of headingsSpans){
+        headingSpan.remove();
     }
 }
