200a092aa2b73e5db155c0021be8f1190af95d4f
Tom Gregan <tgregan90@hotmail.co.uk>
2021-03-19 11:23:33 +0000

    Updated to include robots and canonical tags

--- meta-copy-modal.js
+++ meta-copy-modal.js
@@ -1,17 +1,19 @@
 document.querySelectorAll(".metaBlockModal").forEach(block => block.remove());
 let modalElement = `
-<div class="metaBlockModal" style="">
-
-<span class="metaBlockModal-closeButton" style=""></span>
-
+<div class="metaBlockModal" >
+<span class="metaBlockModal-closeButton" ></span>
 <p><strong>Title</strong>: </p>
 <p>${document.title} </p>
 <p>Word Count: <span class="titleCount"></span></p>
-
 <p> <strong>Meta Description</strong>: </p>
 <p>${document.querySelector("meta[name='description']").content}</p>
 <p>Word Count: <span class="descriptionCount"></span></p>
-
+<p><strong>Meta robots</strong>:</p> 
+<p>${document.querySelector("meta[name='robots']").content}</p>
+<p><strong>Canonical</strong>:</p>
+<p>${document.querySelector("link[rel='canonical']").href} - <span class=${( document.querySelector("link[rel='canonical']").href === window.location.href ) ? "Pass" :  "Fail"}>
+${( document.querySelector("link[rel='canonical']").href === window.location.href ) ?  "Pass" :  "Fail"}</span>
+</p>
 <style>
 .metaBlockModal{
     display: block;
@@ -21,8 +23,6 @@ let modalElement = `
     padding: 30px 0px 20px 30%;
     position:relative;
 }
-
-
 .metaBlockModal-closeButton{
     position: absolute;
     top: 20px;
@@ -33,19 +33,25 @@ let modalElement = `
     cursor:pointer;
     background-color: white;
 }
-
 .metaBlockModal-closeButton:after{
     content: "+";
 }
-
 .metaBlockModal-closeButton:hover{
     transform: rotate(45deg);
     background-color: black;
     color: white;
 }
+.metaBlockModal .Pass{
+    background-color: lightgreen;
+    padding: 10px 20px;
+}
+.metaBlockModal .Fail{
+    background-color:#ffcccb;
+    padding: 10px 20px;
+}
 </style>
 </div>
-`
+`;
 document.body.insertAdjacentHTML("afterBegin",modalElement);
 let titleCount = document.title.length;
 let descriptionCount = document.querySelector("meta[name='description']").content.length;
